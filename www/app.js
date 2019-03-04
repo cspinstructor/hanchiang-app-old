ae8d2edeab24d71d70e96edad086fd463bd77425
paulchin <paulchin20@yahoo.com>
2019-03-04 10:59:25 +0800

    app.js

---
+++ www/app.js
@@ -0,0 +1,49 @@
+// $('#news-content').load('http://www.hanchiangnews.com/en/');
+
+//--- load side panel into index.html ---
+$.ajax('sidepanel.html')
+  .done(function(sidepanel) {
+    $('#mypanel').html(sidepanel);
+  })
+  .fail(function() {
+    console.log('ajax sidepanel error');
+  });
+
+$(document).on('click', '#news', function() {
+  console.log('news clicked');
+  $('[data-role=panel]').panel('close');
+  loadNewsPageDiv();
+  //getNews();
+});
+
+$(document).on('click', '#closepanel', function() {
+  console.log('closepanel clicked');
+  $('[data-role=panel]').panel('close');
+});
+
+//--- replace contents div with news div ---
+function loadNewsPageDiv() {
+  $.ajax('news.html')
+    .done(function(newsdiv) {
+      console.log('ajax news done');
+      $('.contents').replaceWith(newsdiv);
+      getNews();
+    })
+    .fail(function() {
+      console.log('ajax unable to replace with news div');
+    });
+}
+
+//--- Hanchiang News ---
+function getNews() {
+  var url = 'http://192.168.137.84/hanchiang/hanchiangscraper.php';
+
+  $.ajax(url)
+    .done(function(data) {
+      console.log(data);
+      $('.contents').html(data);
+    })
+    .fail(function() {
+      $('.contents').html(name + ' not found!');
+    });
+}
