b515adc80fe5fc711355a1185b536cd5137fcdf4
paulchin <paulchin20@yahoo.com>
2019-03-07 17:51:39 +0800

    WordPress REST API

--- platforms/browser/www/app.js
+++ platforms/browser/www/app.js
@@ -33,24 +33,40 @@ function loadNewsPageDiv() {
 
 //--- Hanchiang News ---
 function getNews() {
-  var url = "http://hjuapp.site/php/getnews.php";
-
-  var output = "";
-
-  $.getJSON(url, function(result) {
-    console.log(result);
-    $.each(result, function(i, field) {
-      output += "<li class='wrap'>";
-      output += "<strong>" + field.title + "</strong><br>";
-      output += field.date + "<br>";
-      output += "<img src='";
-      output += field.pic + "'/><br>";
-      output += field.content + "<br>";
-      output += "</li>";
+  const apiRoot = "https://hjuapp.site/wp-json";
+  var wp = new WPAPI({ endpoint: apiRoot });
+  wp.posts()
+    .perPage(3)
+    .then(posts => {
+      posts.forEach(function(post) {
+        console.log(post.content.rendered);
+      });
+    })
+    .catch(err => {
+      console.log("Error: " + err);
     });
-    $("#newslist").append(output);
-    $("#newslist")
-      .listview()
-      .listview("refresh");
-  });
 }
+
+//--- Hanchiang News ---
+// function getNews() {
+//   var url = "http://hjuapp.site/php/getnews.php";
+
+//   var output = "";
+
+//   $.getJSON(url, function(result) {
+//     console.log(result);
+//     $.each(result, function(i, field) {
+//       output += "<li class='wrap'>";
+//       output += "<strong>" + field.title + "</strong><br>";
+//       output += field.date + "<br>";
+//       output += "<img src='";
+//       output += field.pic + "'/><br>";
+//       output += field.content + "<br>";
+//       output += "</li>";
+//     });
+//     $("#newslist").append(output);
+//     $("#newslist")
+//       .listview()
+//       .listview("refresh");
+//   });
+// }
